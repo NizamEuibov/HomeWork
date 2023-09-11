c2f61b44dc9e6535119b8b64988e61442d44b226
Nizam <nizam.eyyubov@mail.ru>
2023-09-11 17:55:06 +0400

    first project

---
+++ app/src/main/java/com/example/adroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.adroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.practice2_1)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Adroid"
+include(":app")
+include(":mylibrary")
